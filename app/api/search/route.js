116f8d3b1b16b93d7171902f287b951fa4d80e49
SATY4JEET <89188773+SATY4JEET@users.noreply.github.com>
2023-08-16 13:05:50 +0530

    Add files via upload

---
+++ app/api/search/route.js
@@ -0,0 +1,30 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  // // Replace the uri string with your connection string.
+  const query = request.nextUrl.searchParams.get("query");
+  const uri = "mongodb+srv://svmore456:svmore456@cluster0.a49w601.mongodb.net/";
+
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const products = await inventory
+      .aggregate([
+        {
+          $match: {
+            $or: [{ slug: { $regex: query, $options: "i" } }],
+          },
+        },
+      ])
+      .toArray();
+    return NextResponse.json({ success: true, products });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
