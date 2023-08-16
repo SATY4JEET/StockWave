a3e5bb601acc931324268ba2c3a997075d486f4f
SATY4JEET <89188773+SATY4JEET@users.noreply.github.com>
2023-08-16 13:04:23 +0530

    Add files via upload

---
+++ app/action/route.js
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  // // Replace the uri string with your connection string.
+  let { action, slug, initialquantity } = await request.json();
+  const uri = "mongodb+srv://svmore456:svmore456@cluster0.a49w601.mongodb.net/";
+
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("stock");
+    const inventory = database.collection("inventory");
+    const filter = { slug: slug };
+
+    let finalquantity =
+      action == "plus"
+        ? parseInt(initialquantity) + 1
+        : parseInt(initialquantity) - 1;
+    const updateDoc = {
+      $set: {
+        quantity: finalquantity,
+      },
+    };
+    const result = await inventory.updateOne(filter, updateDoc);
+
+    return NextResponse.json({
+      success: true,
+      message: `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)}`,
+    });
+  } finally {
+    await client.close();
+  }
+}
